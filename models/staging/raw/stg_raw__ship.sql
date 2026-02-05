6dffb27c8d6a0cd170e01ab8399ffa192400edcf
fabrice waymel <fabrice.waymel@gmail.com>
2026-02-05 11:13:24 +0000

    correction des bug

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,12 +1,11 @@
 with
-    source as (select * from {{ source("raw", "ship") }}),
-    renamed as (
-        select 
-            orders_id,
-            shipping_fee, 
-            logcost, 
-            cast(ship_cost as float64) as ship_cost
-        from source
-    )
-select *
-from renamed
+source as (select * from {{ source('raw', 'ship') }}),
+  renamed as (
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        CAST(ship_cost AS INT64) AS ship_cost
+    from source
+)
+select * from renamed
