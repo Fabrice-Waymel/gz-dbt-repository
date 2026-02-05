473d6f3c7989d49e15aaa061864af2e6f2eec940
fabrice waymel <fabrice.waymel@gmail.com>
2026-02-05 14:15:50 +0000

    correction

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,12 +1,19 @@
-with 
+with
+
 source as (
+
     select * from {{ source('raw', 'product') }}
+
 ),
+
 renamed as (
-    select 
+
+    select
         products_id,
-        cast(purchSE_PRICE as float64) as purchase_price
-        
+        CAST(purchse_price as FLOAT64) as purchase_price
+
     from source
+
 )
+
 select * from renamed
